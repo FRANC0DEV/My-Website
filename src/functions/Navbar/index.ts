0775bd352502b1c9fa77bfb2f7f3966be40491ef
Edgar Franco <just.me.edgar.2004@gmail.com>
2026-02-13 12:23:41 -0500

    Fix styles on navbar

--- src/functions/Navbar/index.ts
+++ src/functions/Navbar/index.ts
@@ -1,10 +1,4 @@
-import getElement from "../../utils/getElement";
 import InitializeThemeToggle from "./theme-toggle";
 export function setupNavbar() {
-  const navbarToggle = getElement("navbar-menu-toggle", HTMLButtonElement);
-  const menu = getElement("navbar-menu", HTMLUListElement);
-  navbarToggle.addEventListener("click", () => {
-    menu.classList.toggle("translate-y-full");
-  });
   InitializeThemeToggle();
 }
