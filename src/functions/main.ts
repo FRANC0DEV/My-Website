6fd05b0cd1b8b4a2181d971bde1d5149086cc56c
Edgar Franco <just.me.edgar.2004@gmail.com>
2026-02-21 13:46:48 -0500

    Lighthouse(fix): Layout shift due to font change eliminated

--- src/functions/main.ts
+++ src/functions/main.ts
@@ -1,8 +1,15 @@
+// Import fonts FIRST
+import '@fontsource/quicksand/300.css';
+import '@fontsource/quicksand/400.css';
+import '@fontsource/quicksand/700.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/julius-sans-one/400.css';
+// Then your CSS
+import '../../index.css';
+
 import { setupNavbar } from "./navbar";
 import { setupHeroSection } from "./hero";
 import { setupSkillsAndTechSection } from "./skills-and-technologies";
-//elements
-
 setupNavbar();
 setupHeroSection();
 setupSkillsAndTechSection();
